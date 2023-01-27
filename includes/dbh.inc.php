74b4d31781253548cf57347fa38c5eb558d7b69c
andrewlapointe <andrewrlapointe@gmail.com>
2023-01-27 13:37:19 -0800

    misc

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -2,7 +2,7 @@
 
 $dbServername = "localhost";
 $dbUsername = "root"; // Xamp default
-$dbPassword = "G%83wViog6b1"; // Xamp default
+$dbPassword = "G%83wViog6b1"; // LastPass 
 $dbName = "phptest";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
