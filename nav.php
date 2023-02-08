85e91d5ded1e618ca0b49b7058bb1faaf1004432
andrewlapointe <andrewrlapointe@gmail.com>
2023-02-08 12:08:46 -0800

    test

--- nav.php
+++ nav.php
@@ -1,24 +1,21 @@
-<head>
-    <link rel="stylesheet" href="css/navbar.css" />
-</head>
 
 <header>
       <div class="navbar" style="z-index: 10">
-        <a class='logo' href="index.php">Mom's Cookbook</a>
-        <a class='shade' href="month_view.php">Month View</a>
-        <a class='shade' href="dummy.php">Dummy
-        <a class='shade' href="about.php">About</a>
-
-        <!-- Search Bar #TODO -->
-        <input type="text" placeholder="Search..">
-        
+        <div id="nav-links">
+          <a class='shade' href="index.php">Mom's Cookbook</a>
+          <a class='shade' href="month_view.php">Month View</a>
+          <a class='shade' href="dummy.php">Dummy
+          <a class='shade' href="about.php">About</a>
+          </div>
+          <!-- Search Bar #TODO -->
+          <input type="text" placeholder="Search..">
+          <div id="right-link">
+            <a href="https://github.com/andrewlapointe">GitHub</a>
+          </div>
+          
+        </div>
         <!-- <a href=""></a> -->
-        <button
-          onclick="window.location.href='https://github.com/andrewlapointe';"
-          class="btn"
-          style="z-index: 12"
-        >
-          GitHub
-        </button>
+        
+
       </div>
 </header>
