e41f56679c884605aa2bc164e75ac65273e5ef57
andrewlapointe <andrewrlapointe@gmail.com>
2023-03-12 12:45:22 -0600

    removed socials from footer

--- footer.php
+++ footer.php
@@ -4,16 +4,6 @@
     <div class="footer" id="footer-container">
         <p>LaPointe Cooking Compendium &copy; 2023 - Andrew LaPointe</p>
         <p><a href="site-plan.php" target="_blank">Site Plan</a></p>
-        <p><a href="https://www.google.com/imgres?imgurl=https%3A%2F%2Ffreight.cargo.site%2Fw%2F500%2Fi%2Fb334a88e29d8d253a49bc3baf614e909913c7aaf1d65b5f7d963aae0fbb267ec%2FSuggestionBox-gif.gif&imgrefurl=https%3A%2F%2Fnew-studio.us%2FSuggestion-Box&tbnid=_djT_DRHFfoOmM&vet=12ahUKEwj0xoSr9tT9AhVeOkQIHT_QAZsQMygAegUIARDcAQ..i&docid=sSdJrAIqxrQ9FM&w=500&h=500&q=suggestion%20box%20shredder%20gif&ved=2ahUKEwj0xoSr9tT9AhVeOkQIHT_QAZsQMygAegUIARDcAQ"
-                target="_blank">Contact Us</a></p>
-        <div class="social">
-            <a href="https://facebook.com" target="_blank">
-                <img src="images/facebook.png" alt="fb icon" />
-            </a>
-            <a href="https://twitter.com" target="_blank">
-                <img src="images/twitter.png" alt="twitter icon" />
-            </a>
-            <a href="https://instagram.com" target="_blank">
-                <img src="images/instagram.png" alt="instagram icon" />
-        </div>
+        <p><a href="https://github.com/andrewlapointe">GitHub</a></p>
+    </div>
 </footer>
