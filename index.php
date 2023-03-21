5538dcbcc8c11f5700facf17b58fd20f4bfe364d
andrewlapointe <andrewrlapointe@gmail.com>
2023-03-21 14:21:47 -0600

    fixed sample cards, changed colors

--- index.php
+++ index.php
@@ -9,45 +9,35 @@
         <h1>The <span class="italic">Last</span> Cookbook You'll Ever Need.</h1>
     </div>
     <div id="sample-container">
-        <div class="sample-item">
-            <a href="recipe.php">
-                <img class="sample-image" src="images/th-928319788" alt="sample image">
-            </a>
-            <div class="break"></div>
-            <h3>Sample Recipe</h3>
-        </div>
-        <div class="sample-item">
-            <a href="recipe.php">
-                <img class="sample-image" src="images/th-928319788" alt="sample image">
-            </a>
-            <div class="break"></div>
-            <h3>Sample Recipe</h3>
-        </div>
-        <div class="sample-item">
-            <a href="recipe.php">
-                <img class="sample-image" src="images/th-928319788" alt="sample image">
-            </a>
-            <div class="break"></div>
-            <h3>Sample Recipe</h3>
+        <div id="sample-cards">
+            <div class="sample-item">
+                <a href="recipe.php">
+                    <img class="sample-image" src="images/th-928319788" alt="sample image">
+                </a>
+                <div class="break"></div>
+                <h3>Sample Recipe</h3>
+            </div>
+            <div class="sample-item">
+                <a href="recipe.php">
+                    <img class="sample-image" src="images/th-928319788" alt="sample image">
+                </a>
+                <div class="break"></div>
+                <h3>Sample Recipe</h3>
+            </div>
+            <div class="sample-item">
+                <a href="recipe.php">
+                    <img class="sample-image" src="images/th-928319788" alt="sample image">
+                </a>
+                <div class="break"></div>
+                <h3>Sample Recipe</h3>
+            </div>
         </div>
     </div>
+
 </main>
 <?php
         include 'footer.php';
     ?>
 </body>
-
-
-<!-- HIDDEN -->
-<!-- <div style="visibility: hidden;">
-    <?php
-    echo 'PHP version: ' . phpversion();
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-      }
-      echo "Connected successfully";
-    ?>
-</div> -->
-<!-- HIDDEN -->
 
 </html>
