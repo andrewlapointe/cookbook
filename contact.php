fd8e3bd0ba4f10da4c3dcaa6b0ac03a328ebbd5b
andrewlapointe <andrewrlapointe@gmail.com>
2023-03-23 20:06:26 -0600

    fixed image sizing

--- contact.php
+++ contact.php
@@ -1,8 +1,9 @@
 <?php
     include 'nav.php';
 ?>
-
-<img src="images/SuggestionBox-gif.gif" alt="shredder">
+<div style="max-height: 1vh; text-align: center; margin-top: 20px;">
+    <img src="images/SuggestionBox-gif.gif" alt="shredder">
+</div>
 
 <?php
         include 'footer.php';
