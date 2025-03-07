636b37e0b81dc097c92e48599094dee2452ad3d6
Andrew LaPointe <andrewrlapointe@gmail.com>
2025-03-06 19:18:54 -0700

    removed test flash message

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 
 baseController.buildHome = async function (req, res) {
     //   const nav = await utilities.getNav();
-    req.flash('notice', 'This is a flash message.');
+    // req.flash('notice', 'This is a flash message.');
 
     const result = await cookbookModel.allRecipes();
 
